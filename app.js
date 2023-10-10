cbd1e20bc23e181f7356f8ec3ed979e97e55f3dc
Sadik Shaikh <abdullahsadik00@gmail.com>
2023-10-10 23:40:49 +0530

    Responding to URL Parameters

--- app.js
+++ app.js
@@ -35,8 +35,20 @@ app.post('/api/v1/tours', (req, res) => {
 
 app.post('/api/v1/tours/:id', (req, res) => {
   console.log(req.params);
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+  if (!tour) {
+    res.status(404).json({
+      status: 'fail',
+      message: 'Invaid ID',
+    });
+  }
+
   res.status(200).json({
     status: 'success',
+    data: {
+      tour,
+    },
   });
 });
 const port = 3000;
